910828d5f09777eb458d9c8f5fd64dd1dd900a00
wjx <wjx-1@outlook.com>
2019-11-27 08:55:32 +0800

    1

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,7 +1,7 @@
 class Sl extends Thread{
     private int ticket = 500;
     public void run(){
-        while (ticket!=0){
+        while (ticket>=0){
             System.out.println(Thread.currentThread().getName()+" ticket = " + ticket--);
             try {
                 Thread.sleep(100);
@@ -14,7 +14,7 @@ class Sl extends Thread{
 class S implements Runnable{
     private int ticket = 500;
     public void run(){
-       while (ticket!=0){
+       while (ticket>=0){
                 System.out.println(Thread.currentThread().getName()+" ticket = " + ticket--);
                 try {
                     Thread.sleep(100);
@@ -27,7 +27,7 @@ class S implements Runnable{
 class Nomal {
     static void run(){
         int ticket = 500;
-        while (ticket!=0){
+        while (ticket>=0){
             System.out.println(Thread.currentThread().getName()+" ticket = " + ticket--);
             try {
                 Thread.sleep(100);
@@ -42,14 +42,15 @@ public class ThreadTest{
     public static void main(String[] args){
         boolean i = true;
         S my = new S();
-        Thread a = new Thread(my);
-        Thread b = new Thread(my);
-        Thread c = new Thread(my);
-        Thread d = new Thread(my);
-        long start=System.currentTimeMillis();   //获取开始时间
-        a.start();b.start();
-        c.start();d.start();
-        //e.start();f.start();g.start();h.start();
+        long start=System.currentTimeMillis();//获取开始时间
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
+        new Thread(my).start();
         while (i){
             if(Thread.activeCount()==2)
             {
